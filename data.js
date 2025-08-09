5bdc1626b14a082fca6fa7af15d2ea988efd982c
Naimish-2026 <rustbooks.yt@gmail.com>
2025-08-09 22:37:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,19 +4,19 @@ const books = [
   {
     title: "The Creative Act: A Way of Being",
     description: "A masterfully written book about the creative process. Rick Rubin, a legendary music producer, shares his wisdom on what it means to be an artist and how to cultivate creativity in any field.",
-    imageSrc: "https://m.media-amazon.com/images/I/71B-QyZ1H-L._SL1500_.jpg", // Replace with your image path
+    imageSrc: "assets/1.png", // Replace with your image path
     audioSrc: "assets/1.mp3"  // Replace with your audio file path
   },
   {
     title: "Atomic Habits",
     description: "An easy and proven way to build good habits and break bad ones. James Clear presents a practical framework for improving every day, no matter your goals.",
-    imageSrc: "https://m.media-amazon.com/images/I/81YkqyaFVEL._SL1500_.jpg", // Replace with your image path
+    imageSrc: "assets/1.png", // Replace with your image path
     audioSrc: "assets/1.mp3"  // Replace with your audio file path
   },
   {
     title: "Sapiens: A Brief History of Humankind",
     description: "A groundbreaking narrative of humanity's creation and evolution that explores the ways in which biology and history have defined us and enhanced our understanding of what it means to be 'human'.",
-    imageSrc: "https://m.media-amazon.com/images/I/713jIoMO3UL._SL1500_.jpg", // Replace with your image path
+    imageSrc: "assets/1.png", // Replace with your image path
     audioSrc: "assets/1.mp3"  // Replace with your audio file path
   }
 ];
